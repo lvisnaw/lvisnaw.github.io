6733bf54ad8ade595f5905f5e5dcaefcd272f31b
lvisnaw <lvisnaw@gmail.com>
2021-02-28 00:29:03 -0700

    Fixing message center

--- lesson8/js/scripts.js
+++ lesson8/js/scripts.js
@@ -14,8 +14,6 @@ const options1 = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric
 document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options1);
 const options2 = {year: 'numeric'};
 document.getElementById('currentYear').textContent = new Date().toLocaleDateString('en-US', options2);
-const options3 = {month: 'numeric', day: 'numeric', year: 'numeric'};
-document.getElementById('numericDate').textContent = new Date().toLocaleDateString('en-US', options3);
 
 const today = new Date();
 console.log(today);
@@ -29,8 +27,4 @@ if (dayNumber == 5) {
     element.classList.add("showme");
 } else {
     element.classList.add("hideme");
-}
-
-function adjustRating(rating) {
-    document.getElementById("ratingvalue").innerHTML = rating;
 }
