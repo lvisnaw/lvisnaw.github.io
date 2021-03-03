61c8b2ea7df83ff2ba55f21c8a2f9feb374c6d70
lvisnaw <lvisnaw@gmail.com>
2021-03-03 11:19:47 -0700

    Fixing datalist

--- lesson8/js/rate.js
+++ lesson8/js/rate.js
@@ -1,11 +1,3 @@
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
-
-function selectResponse() {
-	const s = document.querySelector('#selected')
-	const sel = document.querySelector('#stormSeverity');
-	s.style.display = "block";
-	s.textContent = sel.value;
-	
-}
