3b21895dfbf1319125d93fc344691af323c52b76
lvisnaw <lvisnaw@gmail.com>
2021-03-02 22:20:58 -0700

    fixed storm conditions

--- lesson8/js/rate.js
+++ lesson8/js/rate.js
@@ -1,3 +1,11 @@
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
+
+function selectResponse() {
+	const s = document.querySelector('#selected')
+	const sel = document.querySelector('#stormSeverity');
+	s.style.display = "block";
+	s.textContent = sel.value;
+	
+}
