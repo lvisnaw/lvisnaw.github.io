ca3eb0281eea56a06f7c580122aead4a87b73713
Leon Visnaw <71676947+lvisnaw@users.noreply.github.com>
2022-04-22 21:44:54 -0600

    Test

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+const options1 = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options1);
+const options2 = {year: 'numeric'};
+document.getElementById('currentYear').textContent = new Date().toLocaleDateString('en-US', options2);
